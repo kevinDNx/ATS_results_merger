4b72c56058137a084ece8cdfb0fdba79dd909874
Kevin D. Nguyen <90806410+kevinDNx@users.noreply.github.com>
2022-07-01 11:46:00 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import glob
 import pandas as pd
 
 
@@ -12,12 +13,27 @@ def list_files(filepath, filetype):
 
 
 csv_list = list_files('ATC Result 2022-0613', "csv")
-print(csv_list)
-
+#TODO: insert function to sort csv_list by date
+#print(csv_list)
+snum_list = []
 for scan in csv_list:
     path_parse = scan.split("/")
-    #path_parse
+    # path_parse
     parsed = path_parse[2].split("_")
-    print(parsed)
-
-print(csv_list)
+    # print(parsed)
+    snum = parsed[0]
+    if snum not in snum_list:
+        snum_list += [snum]
+        merge_list = list(filter(lambda x: snum in x, csv_list))
+        press_list = list(filter(lambda x: 'press' in x, merge_list))
+        temp_list = list(filter(lambda x: 'Temp' in x, merge_list))
+        #print(merge_list)
+        #print(press_list)
+        #print(temp_list)
+        #TODO: combine all elements in press_list and temp_list.
+        press_combine = pd.concat([pd.read_csv(f) for f in press_list])
+        #print(press_combine)
+        temp_combine = pd.concat([pd.read_csv(f) for f in temp_list])
+        print(temp_combine)
+        #TODO: make a writer to write the list of csv files to the same output file and sheet
+        # Use snum, press_list, and merge_list.
